780e056085432f3c7cf4bc0251e9ef98659255fb
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-16 12:51:07 -0600

    Update typeDefs to include journal entry data

--- typeDefs.js
+++ typeDefs.js
@@ -3,26 +3,37 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type JournalEntry {
     id: ID
+    userId: String
     date: String
+    waterIntake: Int
+    proteinIntake: Int
+    exercise: Int
+    kegels: Int
+    garlandPose: Int
+    prenatalVitamins: Boolean
+    probiotics: Boolean
   }
 
   type Query {
-    hello: String
-
     getAllJournalEntries: [JournalEntry]
-
     getJournalEntry(id: ID): JournalEntry
   }
 
   input JournalEntryInput {
     date: String
+    userId: String
+    waterIntake: Int
+    proteinIntake: Int
+    exercise: Int
+    kegels: Int
+    garlandPose: Int
+    prenatalVitamins: Boolean
+    probiotics: Boolean
   }
 
   type Mutation {
     createJournalEntry(journalEntry: JournalEntryInput): JournalEntry
-
     deleteJournalEntry(id: ID): String
-
     updateJournalEntry(id: ID, journalEntry: JournalEntryInput): JournalEntry
   }
 `;
