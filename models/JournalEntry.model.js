b6ecbb7af839ebdc41eec549151be16bd80f715f
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-16 12:50:24 -0600

    Update JournalEntrySchema by including the rest of the journal entry data

--- models/JournalEntry.model.js
+++ models/JournalEntry.model.js
@@ -1,10 +1,42 @@
 const mongoose = require('mongoose');
 
 const JournalEntrySchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    }, 
     date: {
         type: String,
         required: true
-    },  
+    }, 
+    waterIntake: {
+        type: Number,
+        required: true
+    },
+    proteinIntake: {
+        type: Number,
+        required: true
+    },
+    exercise: {
+        type: Number,
+        required: true
+    }, 
+    kegels: {
+        type: Number,
+        required: true
+    }, 
+    garlandPose: {
+        type: Number,
+        required: true
+    }, 
+    prenatalVitamins: {
+        type: Boolean,
+        required: true
+    }, 
+    probiotics: {
+        type: Boolean,
+        required: true
+    }, 
 });
 
 const JournalEntry = mongoose.model('journalEntry', JournalEntrySchema);
