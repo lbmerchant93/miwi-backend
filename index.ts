a4b1b7d9dee22008f20d9d80b3f7155425b76eff
Lucas Merchant <lbmerchant93@gmail.com>
2022-03-17 14:04:15 -0600

    Add two entries to database, query them

--- index.ts
+++ index.ts
@@ -1,15 +1,29 @@
 import "reflect-metadata";
-import { PrismaClient } from "./node_modules/.prisma/client/index";
+import { PrismaClient } from "./src/generated/prisma-client/index";
 // need to update this to point to a generated path
-const express = require('express');
-const graphql = require('graphql');
-const { ApolloServer } = require('apollo-server-express');
+// const express = require('express');
+// const graphql = require('graphql');
+// const { ApolloServer } = require('apollo-server-express');
 
 const prisma = new PrismaClient();
 
 async function main() {
   // Connect the client
   await prisma.$connect()
+
+  // await prisma.journalentries.create({
+  //   data: {
+  //     date: "2022-03-08T19:58:57.000Z",
+  //     exercise: 2,
+  //     garlandPose: 2,
+  //     kegels: 2,
+  //     prenatalVitamins: true,
+  //     probiotics: true,
+  //     proteinIntake: 2,
+  //     userId: "fV5De0bivMRqBoHxJuwT4UwFJtT2",
+  //     waterIntake: 2
+  //   }
+  // })
   // ... you will write your Prisma Client queries here
   const allEntries = await prisma.journalentries.findMany()
   console.log(allEntries)
