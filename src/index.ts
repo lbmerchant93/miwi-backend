a0cbf40e45e5c0a1fde1accc1bfe5d33b828fbd3
Lucas Merchant <lbmerchant93@gmail.com>
2022-06-08 12:22:51 -0600

    Add setting res.header

--- src/index.ts
+++ src/index.ts
@@ -63,6 +63,11 @@ async function main() {
   const PORT = process.env.PORT || 9000;
 
   app.use(cors());
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
   app.use(express.json());
   app.use(express.urlencoded({extended: true}));
   app.use(expressAuthnMiddleware);
