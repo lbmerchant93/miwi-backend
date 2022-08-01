85031268eef9dce2cf35e2fbf671e52c60c47a0d
Lucas Merchant <lbmerchant93@gmail.com>
2022-08-01 12:04:52 -0600

    Add new additional journal entry sections

---
+++ prisma/migrations/20220801180253_additional_journal_entry_sections/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "JournalEntry" ADD COLUMN     "childbirthEducation" TEXT,
+ADD COLUMN     "fetalLoveBreak" TEXT,
+ADD COLUMN     "mood" TEXT,
+ADD COLUMN     "postpartumPrep" TEXT,
+ADD COLUMN     "selfCare" TEXT;
